9128e8a4c7e19eae35f2d5ad72aca8a57fd763a6
michaeljohnson8971 <michaeljohnson8971@gmail.com>
2026-03-09 02:24:28 +0000

    index update and dependncy cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Index from "./pages/Index";
-import About from "./pages/About";
-import Services from "./pages/Services";
-import NotFound from "./pages/NotFound";
+import { Header } from "./components";
+import { Footer } from "./components";
+import { Index, About, Services, NotFound } from "./pages";
 
 const queryClient = new QueryClient();
 
---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as About } from "./About";
+export { default as Index } from "./Index";
+export { default as NotFound } from "./NotFound";
+export { default as Services } from "./Services";
